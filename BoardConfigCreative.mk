7324b7164dc327595b59ff06b6785dee9d4d26d9
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-22 21:22:09 +0530

    Add initial Creative product makefiles
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

---
+++ BoardConfigCreative.mk
@@ -0,0 +1,5 @@
+#
+# Copyright (C) 2022 The CreativeOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
---
+++ creative_oriole.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2022 The CreativeOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common Creative stuff.
+$(call inherit-product, vendor/creative/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/raviole/aosp_oriole.mk)
+$(call inherit-product, device/google/gs101/creative_common.mk)
+$(call inherit-product, device/google/raviole/device-creative.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 6
+PRODUCT_NAME := creative_oriole
+
+# Boot animation
+CREATIVE_BOOTANIMATION := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=oriole \
+    PRIVATE_BUILD_DESC="oriole-user 13 TP1A.221105.002 9080065 release-keys"
+
+BUILD_FINGERPRINT := google/oriole/oriole:13/TP1A.221105.002/9080065:user/release-keys
